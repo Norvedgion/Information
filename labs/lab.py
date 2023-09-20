49629cb684858f16f5acdcb491dbea5298033a71
Norvedgion <144201922+Norvedgion@users.noreply.github.com>
2023-09-20 11:19:08 +0500

    сделала задание 1

--- labs/lab.py
+++ labs/lab.py
@@ -1,4 +1,17 @@
-import math
+from math import *
 
 def task1():
   pass
+
+
+x = float(input("введи значение x: "))
+y = float(input())
+z = float(input())
+
+a1=log1p(sqrt(abs(x-1)+1))+sqrt(abs(y))
+a2=sqrt(x**2+x)+sin(y)*cos(x)
+a=a1/a2
+b=sqrt(x)-(abs(z))**(1/4)+4*x-log2(z)
+
+print('a=',a)
+print('b=',b)
