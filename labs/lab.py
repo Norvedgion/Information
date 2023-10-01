2459f3efcec26c937ab2993f1d84dbc291fc27cc
Norvedgion <144201922+Norvedgion@users.noreply.github.com>
2023-10-01 23:14:56 +0500

    Update lab.py

--- labs/lab.py
+++ labs/lab.py
@@ -1,23 +1,18 @@
+import math
 from math import *
 
 def task1():
-  pass
-
-
 x = float(input("введи значение x: "))
 y = float(input())
 z = float(input())
-
 a1=log1p(sqrt(abs(x-1)+1))+sqrt(abs(y))
 a2=sqrt(x**2+x)+sin(y)*cos(x)
 a=a1/a2
 b=sqrt(x)-(abs(z))**(1/4)+4*x-log2(z)
-
 print('a=',a)
 print('b=',b)
 
-from math import *
-def task1():
+def task2():
     print('введите значение x')
     x=float(input())
     a = -3
@@ -28,9 +23,58 @@ def task1():
     t=t1/t2-x**4
     print(t)
 
-
-def task2():
+def task3():
     print('введите значение x')
     x=float(input())
     a=(tan(x))**3*(abs(log1p(x**3)))
-    print(a)
+    print(a) 
+    
+def task4():
+    Sside = float(input("введите площадь боковой поверхности"))
+    Vpyramid = float(input("введите объём пирамиды"))
+    Sbase = float(input("введите площадь квадратного основания"))
+    Hpyramid = (3 * Vpyramid) / Sbase
+    Rcilinder = (Sside / (2 * pi * Hpyramid))
+    Vsilinder = pi * Rcilinder**2 * Hpyramid
+    print(f"Объём цилиндра, {Vsilinder:.4f}")
+
+
+def task5():
+    Money = float(input("введите сколько двое рабочих заработали вместе"))
+    Week_1_Working = 2
+    Week_2_Working = 4
+    Earnings_1 = (Week_1_Working / (Week_1_Working + Week_2_Working )) * Money
+    Earnings_2 = (Week_2_Working / (Week_1_Working + Week_2_Working)) * Money
+    print(f"Первый рабочий заработал, {Earnings_1:.4f} рублей")
+    print(f"Второй рабочий заработал, {Earnings_2:.4f} рублей")
+
+def task6():
+        R = float(input("введите радиус нижнего основания"))
+        r = float(input("введите радиус верхнего основания"))
+        a = float(input("Площадь вертикального сечения"))
+        h = 2 * a / (R + r)
+        v = (pi * h * (R ** 2 + R * r + r ** 2)) / 3
+        print(f"Объём усеченного конуса, {v:.4f}")
+
+
+def task7():
+    x1 = float(input("введите координату x1"))
+    x2 = float(input("введите координату x2"))
+    dist = abs (x2-x1)
+    print(f"расстояние между координатами, {dist:.4f}")
+
+
+def task8():
+    S = float(input("введите путь пройденный телом"))
+    t = float(input("введите время"))
+    N = float(input("введите увеличение скорости"))
+    V = 2 * S / (N * t + t)
+    a = (N * V - V) / t
+    print(f" ускорение, {a:.4f}")
+    print(f" начальная скорость, {V:.4f}")
+
+def task9():
+    KZT= float(input("введите сумму в тенге, котрую хотите перевести"))
+    commission = 2 / 100
+    EUR = KZT / (1977 / 1000000) - commission
+    print(f" сумма с учетом комиссии, {EUR:.2f}")
