724bb81993c49c4234ec88deecbfe2e53d7e108d
Norvedgion <144201922+Norvedgion@users.noreply.github.com>
2024-02-14 01:32:44 +0500

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,28 +1,31 @@
-from PyQt5.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow, QPushButton, QFormLayout, QWidget, QComboBox)
+# Импорт необходимых модулей из библиотек PyQt5 и Matplotlib
+from PyQt5.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow, QPushButton, QVBoxLayout, QWidget, QComboBox)
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-
+# Определение класса главного окна приложения
 class MainWindow(QMainWindow):
+    # Метод инициализации класса
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-
+        # Настройка заголовка окна
         self.setWindowTitle('График')
+        # Создание фигуры и холста для графика Matplotlib
         self.fig = plt.figure()
         self.canvas = FigureCanvas(self.fig)
-
+        # Создание центрального виджета и размещение холста в вертикальном макете
         cental_widget = QWidget()
-        layout = QFormLayout()
+        layout = QVBoxLayout()
         cental_widget.setLayout(layout)
 
         layout.addWidget(self.canvas)
 
         self.setCentralWidget(cental_widget)
-
+        # Создание кнопки для отображения графика
         self.plot_button = QPushButton('Нарисовать график')
         self.plot_button.clicked.connect(self.plot_data)
-
+        # Создание элементов интерфейса для ввода диапазона и функций
         self.range_label = QLabel('Диапазон:')
         self.range_start_input = QLineEdit('0')
         self.range_end_input = QLineEdit('1')
@@ -30,18 +33,18 @@ class MainWindow(QMainWindow):
         self.add_function_button = QPushButton('Добавить функцию в список')
         self.function_input = QLineEdit('*Введите функцию для добавление в список*')
         self.function_widget = QComboBox()
-        self.function_widget.addItems(['x', '2*x', 'x**2', 'x**3'])
+        self.function_widget.addItems(['x', '2**x', 'x**(1/2)', 'x**3'])
         self.add_function_button.clicked.connect(self.add_function)
-
+        # Создание элементов интерфейса для ввода количества точек на графике
         self.point_amount = QLabel('Количество точек на графике:')
-        self.point_input = QLineEdit('50')
-
+        self.point_input = QLineEdit('100')
+        # Создание кнопок для очистки графика и сохранения данных в файл
         self.clear_button = QPushButton('Очистить график')
         self.clear_button.clicked.connect(self.clear_plot)
 
         self.file_button = QPushButton('Сохранить точки в файл')
         self.file_button.clicked.connect(self.file_save)
-
+        # Размещение элементов интерфейса в вертикальном макете
         layout.addWidget(self.function_widget)
         layout.addWidget(self.range_label)
         layout.addWidget(self.range_start_input)
@@ -51,8 +54,10 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.plot_button)
         layout.addWidget(self.clear_button)
         layout.addWidget(self.file_button)
-        layout.addRow(self.add_function_button, self.function_input)
+        layout.addWidget(self.add_function_button)
+        layout.addWidget(self.function_input)
 
+    # Метод для создания массивов x и y для построения графика
     def vectors(self):
         try:
             expression = self.function_widget.currentText()
@@ -80,6 +85,7 @@ class MainWindow(QMainWindow):
 
         return x, y
 
+    # Метод для построения графика на холсте
     def plot_data(self):
 
         x, y = self.vectors()
@@ -88,18 +94,20 @@ class MainWindow(QMainWindow):
         plt.grid(True)
         plt.xlabel('x')
         plt.ylabel('y')
-
+        # Обновление холста для отображения нового графика
         self.centralWidget().layout().itemAt(0).widget().draw()
 
+    # Метод для очистки графика
     def clear_plot(self):
         for ax in self.fig.axes:
             ax.clear()
         plt.grid(True)
         self.canvas.draw()
 
+    # Метод для сохранения данных в файл
     def file_save(self):
         x, y = self.vectors()
-        file = open('dots.txt', 'w')
+        file = open('qwerty.txt', 'w')
         file.write('x     ' + '  ' + 'y\n')
         for i in range(len(x)):
             a, b = map(str, (x[i], y[i]))
@@ -110,12 +118,15 @@ class MainWindow(QMainWindow):
                 b += '0' * (6 - len(b))
             file.write(a + '  ' + b + '\n')
 
+    # Метод для добавления пользовательской функции в выпадающий список
     def add_function(self):
         text_x = self.function_input.text()
         self.function_widget.addItems([text_x])
 
-
+# Создание экземпляра приложения и главного окна
 app = QApplication([])
 main_window = MainWindow()
 main_window.show()
 app.exec()
+
+
